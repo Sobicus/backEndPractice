0506aea0d98a955f7c6dd830555d4311946f46e3
Sobicus <83043123+Sobicus@users.noreply.github.com>
2024-03-01 23:55:37 +0100

    hw#11 refactoring objectResult

--- hw11_refactoring/src/commands/object-result.ts
+++ hw11_refactoring/src/commands/object-result.ts
@@ -1,6 +1,13 @@
 export enum statusType{
+    OK='OK',//200
+    Created='Created',//201
+    NoContent='NoContent',//204
+    BadRequest='BadRequest',//400
+    Unauthorized='Unauthorized',//401
+    Forbidden='Forbidden',//403
+    NotFound='NotFound',//404
+    TooManyRequests='TooManyRequests',//429
     Success='Success',
-    notFound='notFound',
 }
 export type ObjectResult<D>={
     status: statusType
